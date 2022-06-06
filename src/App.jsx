cd5c1135038ae2ebbaf4ba8ccddc66b444731aea
dcartist <pbannerman13@gmail.com>
2022-06-06 10:27:26 -0400

    updating index and installation

--- src/App.jsx
+++ src/App.jsx
@@ -7,37 +7,66 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <fieldset className="space-y-5">
+      <legend className="sr-only">Notifications</legend>
+      <div className="relative flex items-start">
+        <div className="flex items-center h-5">
+          <input
+            id="comments"
+            aria-describedby="comments-description"
+            name="comments"
+            type="checkbox"
+            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+          />
+        </div>
+        <div className="ml-3 text-sm">
+          <label htmlFor="comments" className="font-medium text-gray-700">
+            Comments
+          </label>
+          <p id="comments-description" className="text-gray-500">
+            Get notified when someones posts a comment on a posting.
+          </p>
+        </div>
+      </div>
+      <div className="relative flex items-start">
+        <div className="flex items-center h-5">
+          <input
+            id="candidates"
+            aria-describedby="candidates-description"
+            name="candidates"
+            type="checkbox"
+            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+          />
+        </div>
+        <div className="ml-3 text-sm">
+          <label htmlFor="candidates" className="font-medium text-gray-700">
+            Candidates
+          </label>
+          <p id="candidates-description" className="text-gray-500">
+            Get notified when a candidate applies for a job.
+          </p>
+        </div>
+      </div>
+      <div className="relative flex items-start">
+        <div className="flex items-center h-5">
+          <input
+            id="offers"
+            aria-describedby="offers-description"
+            name="offers"
+            type="checkbox"
+            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+          />
+        </div>
+        <div className="ml-3 text-sm">
+          <label htmlFor="offers" className="font-medium text-gray-700">
+            Offers
+          </label>
+          <p id="offers-description" className="text-gray-500">
+            Get notified when a candidate accepts or rejects an offer.
+          </p>
+        </div>
+      </div>
+    </fieldset>
     </div>
   )
 }
